9abd65f634add8909e53b79012fd6488a66ebe87
javad <javadsaeid8@gmail.com>
2024-08-09 10:44:32 +0330

    add primitive types sample

---
+++ BASICS/PrimitiveTypes.ts
@@ -0,0 +1,12 @@
+// javaScritp types are: string, number, boolean, undefined, null, bigint, symbol
+// these types are valid in typeScript
+
+let testString: string = 'test';
+let testNumber: number = 123;
+let testBool: boolean = false;
+
+// defined variable types can't assing to another types. uncomment below code to see.
+
+// testBool = 133
+// testString = false
+// testNumber = null
