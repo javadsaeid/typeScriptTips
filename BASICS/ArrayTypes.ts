e46f109dea53555a43565baced96529c19a44274
javad <javadsaeid8@gmail.com>
2024-08-26 22:30:08 +0330

    add more example to array types sampel

--- BASICS/ArrayTypes.ts
+++ BASICS/ArrayTypes.ts
@@ -7,3 +7,23 @@ const numbers_: number[] = [1, 2, 3];
 
 numbers_.push(2) // -> it's ok because type is number. but example below will throw error because type can not be string.
 // numbers.push("test") 
+
+
+
+// more advanced example
+
+function convertToNumber (input: number | string): number {
+    if(typeof(input) === 'string') {
+        input = Number.parseInt(input)
+    }
+
+    return input;
+}
+type NumberOrString = number | string;
+
+function getSumOfArr(nums: NumberOrString[]) {
+    return nums.map(n => convertToNumber(n)).reduce((total, value) => total + value, 0);
+}
+
+
+console.log(getSumOfArr([1, "2", "3", 4])); // -> 10
