e8f7c70990c86a61203e6b6133c4cf39bbb54188
javad <javadsaeid8@gmail.com>
2024-08-26 09:27:08 +0330

    type narrowing sample added

---
+++ BASICS/TypeNarrowing.ts
@@ -0,0 +1,16 @@
+// type narrowing is used when we need realize to exact type.
+function buyFunction(stuff: string, count: string | number) {
+    if(typeof(count) === 'string') {
+        return `${count.toLowerCase()} of ${stuff} is ordered`;
+    }
+
+    return `${count} of ${stuff} is ordered`;
+}
+
+// uncomment the below function to see the error!
+// function buyFunction(stuff: string, count: string | number) {
+//     return `${count.toLowerCase()} of ${stuff} is ordered`;
+// }
+
+
+
