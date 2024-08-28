0d04faefa3fef925cfff58fd513caaaf71dfd044
javad <javadsaeid8@gmail.com>
2024-08-28 14:29:25 +0330

    add assign empty array sample

--- BASICS/ArrayTypes.ts
+++ BASICS/ArrayTypes.ts
@@ -1,8 +1,6 @@
 // TypeScript has a specific syntax for typing arrays.
 const numbers: number[] = []; 
-
 // or
-
 const numbers_: number[] = [1, 2, 3];
 
 numbers_.push(2) // -> it's ok because type is number. but example below will throw error because type can not be string.
@@ -11,6 +9,11 @@ numbers_.push(2) // -> it's ok because type is number. but example below will th
 
 
 // more advanced example
+type NumberOrString = number | string;
+
+function getSumOfArr(nums: NumberOrString[]) {
+    return nums.map(n => convertToNumber(n)).reduce((total, value) => total + value, 0);
+}
 
 function convertToNumber (input: number | string): number {
     if(typeof(input) === 'string') {
@@ -19,11 +22,11 @@ function convertToNumber (input: number | string): number {
 
     return input;
 }
-type NumberOrString = number | string;
-
-function getSumOfArr(nums: NumberOrString[]) {
-    return nums.map(n => convertToNumber(n)).reduce((total, value) => total + value, 0);
-}
-
 
 console.log(getSumOfArr([1, "2", "3", 4])); // -> 10
+
+
+// take care of assigning empty array to a variable
+const Arr: [] = [];
+// the code below will throw error
+// Arr.push(2);
