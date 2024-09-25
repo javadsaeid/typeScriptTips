68af543b83c94d96eee5a3f10dc017dfe5d69917
javad <javadsaeid8@gmail.com>
2024-09-25 15:26:02 +0330

    array generic sample added

---
+++ GENERICS/ArrayGenerics.ts
@@ -0,0 +1,19 @@
+// array generic is a clean way to create arrays in typescript
+let someNumber: number[] = [1, 2, 3];
+let someNumber_: Array<number> = [1, 2, 3];
+
+// for array generic using other types are also valid.
+interface TestInterface {
+    name: string;
+    date: Date;
+}
+let cosutomeArrayG: Array<TestInterface>;
+
+// also union types.
+let customeArrayGUnion: Array<TestInterface|number|string>;
+
+customeArrayGUnion = [
+    1,
+    'test string',
+    {name: "some name", date: new Date()}
+];
